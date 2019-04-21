47a94c6d5bcb3e692188c09a36ea65737a5ebe19
Issa Issa <iissa3339@gmail.com>
2019-04-21 00:07:16 -0700

    add bot

---
+++ bot.js
@@ -0,0 +1,25 @@
+var tmi = require('tmi.js')
+
+var options = {
+   options: {
+      debug: true
+},
+   connection: {
+      cluster: "aws",
+      reconnect: true
+},
+   identity: {
+      username: "beachhack_bot",
+      password: "oauth:86x30f7gpo4an5imklo42lhbzi2bwn"
+},
+   channels: ["beachhack_bot"]
+};
+
+var client = new tmi.client(options);
+
+
+client.connect();
+
+client.on('connected', function(address,port){
+    client.action("beachhack_bot","POGGERS s in the chat");
+});
