b894db7d94e89554062714c2e5d16c1c99595b09
jacobpradels <pradelsjacob@gmail.com>
2019-04-21 10:07:03 -0700

    xml change

--- main.py
+++ main.py
@@ -4,59 +4,64 @@ from calculatesalt import determineSalt
 from switch import switch
 from twitchBot import POGGERS
 import time
+import os
+
 emotionData = []
 prevEmotion = [0, 0, 0]
 '''
-This function updates the displayimg.png in /current-img/
+This function updates the displayimg.gif in /current-img/
 '''
+
+
 def switchBar(emotionInfo):
-    path2 = "C:\\Users\\Jake\\Documents\\beachhacks2019\\googlecloudapi\\beachhacks-2019\\current-img"
-    # if (emotionInfo[0] != prevEmotion[0]):
-    #     switch(r"C:\Users\Jake\Documents\beachhacks2019\googlecloudapi\beachhacks-2019\img-src\%d.png"%round(emotionInfo[0]),path2, "display0.png")
-    #     print(round(emotionInfo[0]))
-    #     prevEmotion[0] = emotionInfo[0]
-    # if (emotionInfo[1] != prevEmotion[1]):
-    #     switch(r"C:\Users\Jake\Documents\beachhacks2019\googlecloudapi\beachhacks-2019\img-src\%d.png"%round(emotionInfo[1]),path2, "display1.png")
-    #     print(round(emotionInfo[1]))
-    #     prevEmotion[1] = emotionInfo[1]
-    #     if (emotionInfo[1] == 4):
-    #         print('we here')
-    # if (emotionInfo[2] != prevEmotion[2]):
-    #     switch(r"C:\Users\Jake\Documents\beachhacks2019\googlecloudapi\beachhacks-2019\img-src\%d.png"%round(emotionInfo[2]),path2, "display2.png")
-    #     print(round(emotionInfo[2]))
-    #     prevEmotion[2] = emotionInfo[2]
-    switch(r"C:\Users\Jake\Documents\beachhacks2019\googlecloudapi\beachhacks-2019\img-src\%d.png"%round(emotionInfo[0]),path2, "display0.png")
-    switch(r"C:\Users\Jake\Documents\beachhacks2019\googlecloudapi\beachhacks-2019\img-src\%d.png"%round(emotionInfo[1]),path2, "display1.png")
-    switch(r"C:\Users\Jake\Documents\beachhacks2019\googlecloudapi\beachhacks-2019\img-src\%d.png"%round(emotionInfo[2]),path2, "display2.png")
+    path2 = r"C:\Users\Jake\Documents\beachhacks2019\googlecloudapi\beachhacks-2019"
+    os.system("cd "+ path2)
+    if (emotionInfo[0] != prevEmotion[0]):
+        switch(r"%d" % round(emotionInfo[0]), path2, "display0")
+
+        prevEmotion[0] = emotionInfo[0]
+    if (emotionInfo[1] != prevEmotion[1]):
+        switch(r"%d" % round(emotionInfo[1]), path2, "display1")
+
+        prevEmotion[1] = emotionInfo[1]
+
+    if (emotionInfo[2] != prevEmotion[2]):
+        switch(r"%d" % round(emotionInfo[2]), path2, "display2")
+
+        prevEmotion[2] = emotionInfo[2]
+    # os.system("cd " + path2)
+    # switch(r"%d"%round(emotionInfo[0]), path2, "display0")
+    # switch(r"%d"%round(emotionInfo[1]), path2, "display1")
+    # switch(r"%d"%round(emotionInfo[2]), path2, "display2")
 
 
 # def botSelection(emotionInfo):
-    # if (emotionInfo[1] > 2):
-        # POGGERS()
+# if (emotionInfo[1] > 2):
+# POGGERS()
 
 def main():
     take_a_pic()
     try:
         emotionInfo = []
-        #detect faces returns a list of the face objects
+        # detect faces returns a list of the face objects
         frame = detect_faces("frame0.jpg")
 
-        #This loop is to seperate the list of faces and add them one by one into the list of emotion data
+        # This loop is to seperate the list of faces and add them one by one into the list of emotion data
         for people in frame:
             # if (len(emotionData) >= 10):
             #     emotionData.append(people)
             #     emotionData.pop(0)
             # else:
             emotionData.append(people)
-        #Prints the total amount of faces we have in our data
+        # Prints the total amount of faces we have in our data
         # print(len(emotionData))
-        #Goes through every face and calculates its data
+        # Goes through every face and calculates its data
         for g in emotionData:
             # print(g.anger,g.joy,g.surprise)
-            #Determine salt returns a list in the form [angervalue, joyvalue, surprisevalue]
+            # Determine salt returns a list in the form [angervalue, joyvalue, surprisevalue]
             emotionInfo = determineSalt(emotionData)
         print(emotionInfo)
-        #This function updates all of the gauges for emotions
+        # This function updates all of the gauges for emotions
         try:
             switchBar(emotionInfo)
             # botSelection(emotionInfo)
@@ -64,7 +69,7 @@ def main():
             print('list empty')
     except FileExistsError:
         print('no faces')
-    
+
 
 while (True):
     main()
--- switch.py
+++ switch.py
@@ -1,5 +1,4 @@
-import glob
-import shutil
+
 import os
 from PIL import Image
 
@@ -8,6 +7,14 @@ from PIL import Image
 
 # path2 = "C:\\Users\\Jake\\Documents\\beachhacks2019\\googlecloudapi\\beachhacks-2019\\current-img"
 def switch(displayimg, location, name):
-    logo = Image.open(displayimg)
-    # logo.save(os.path.join(location,name), save_all=True)
-    logo.save(os.path.join(location,name))
+    logo = Image.open(os.path.join(location,displayimg+".gif"))
+    logo.save(os.path.join(location,name+".gif"), save_all=True)
+    # logo.save(os.path.join(location,name+"-temp"))
+    # logo.close()
+    # os.system("cd")
+    # print(location)
+    # os.system("rename "+"\\"+displayimg+"-temp.gif %s.gif"%name)
+    # print("rename "+displayimg+"-temp.gif %s.gif"%name)
+    os.system("move "+"%s.gif"%name + " current-img")
+    print("move "+"%s.gif"%name + " current-img")
+    
